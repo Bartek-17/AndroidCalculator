b21baa403c29be942ef6ee4b3f52a899bb6d8656
Bartek-17 <bartosz.pelka12@gmail.com>
2026-01-18 18:51:54 +0100

    Delete AndroidCalculator/app directory

--- AndroidCalculator/app/src/main/java/com/example/androidcalculator/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package com.example.androidcalculator.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val DarkerBlue = Color(0xFF143B59)
-val LightBlue = Color(0xFF29A1CB)
-val DarkBlueBackground = Color(0xFF081F33)
